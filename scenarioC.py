6938560fb872e50d77dbdf26079908b54081f65b
Dhorasso <115734874+Dhorasso@users.noreply.github.com>
2025-01-30 20:05:20 +0000

    Update scenarioC.py

--- scenarioC.py
+++ scenarioC.py
@@ -118,11 +118,14 @@ simulated_data
 ############ SEPTP 2: Define your observation distribution ######################################
 # The observation_dist.py contains some examples, you extend to incorporate multiple dataste
 #################################################################################################
-# Poisson log-likelihood
-def obs_dist_poisson(observed_data, model_data, theta, theta_names):
+
+# Negative Binomial log-likelihood
+def obs_dist_negative_binomial(observed_data, model_data, theta, theta_names):
     epsi = 1e-4
     model_est_case = np.maximum(epsi, model_data['NI'].to_numpy())
-    log_likelihoods = poisson.logpmf(observed_data['obs'], mu=model_est_case)
+    param = dict(zip(theta_names, theta))
+    overdispersion = param.get('phi', 0.1)  # Default value for 'phi' if not provided
+    log_likelihoods = nbinom.logpmf(observed_data['obs'], 1 / overdispersion, 1 / (1 + overdispersion * model_est_case))
     log_likelihoods[np.isnan(log_likelihoods) | np.isinf(log_likelihoods)] = -np.inf
     return log_likelihoods
 
@@ -136,8 +139,8 @@ np.random.seed(123) # Set a seed for reproducibility
 # # ##### # setting state and parameter
 
 ### SIR initial state and prior distribution
-N_pop=2e5
-state_info_seir = {
+N_pop=5e5
+state_info_sir = {
     'S': {'prior': [N_pop-3, N_pop, 0, 0, 'uniform']},  # Susceptibles
     'I': {'prior': [0, 3, 0, 0, 'uniform']},  # Infected
     'R': {'prior': [0, 0, 0, 0, 'uniform']},  # Removed (Recovered or Deceased)
@@ -145,7 +148,7 @@ state_info_seir = {
     'B': {'prior': [0, np.inf, 0.725, 0.01, 'normal']},  # Transmission rate (Beta)
 }
 
-theta_info_seir = {
+theta_info_sir = {
     'gamma': {'prior': [0.1, 0.2, 0.16, 0.1,'truncnorm','log']},  # Removal rate (Inverse of infectious period)
     'nu_beta': {'prior': [0.05, 0.15,0.1,0.05, 'truncnorm','log']},  # Standard deviation of RW process (Beta variability)
     'phi': {'prior': [1e-5, 0.1,0,0, 'uniform','log']}  # Overdispersion parameter
@@ -166,21 +169,21 @@ theta_info_dthp = {
 
 
 
-days=t_end-14
 fday=14
+days=len(simulated_data)-fday
+
 
 smc2_results = BMA_SMC2(
     model_sir=stochastic_sir_model,
-    model_dthp= dthp_model_flu, 
+    model_dthp= dthp_model, 
     initial_state_info_dthp=state_info_dthp , 
     initial_theta_info_dthp=theta_info_dthp , 
-    model_seir=stochastic_model_sir, 
-    initial_state_info_seir=state_info_seir , 
-    initial_theta_info_seir=theta_info_seir , 
-    observed_data=simulated_data[:days+1],
+    initial_state_info_sir=state_info_sir , 
+    initial_theta_info_sir=theta_info_sir , 
+    observed_data=simulated_data[:days],
     num_state_particles=200,
     num_theta_particles=400,
-    observation_distribution=obs_dist_poisson,
+    observation_distribution=obs_dist_negative_binomial,
     forecast_days=fday,
 )
 
@@ -188,24 +191,25 @@ smc2_results = BMA_SMC2(
 
 
 ##########################################################################################################
-########## SETP4: Visualize the Results ####################################################################
+########## SETP4: Visualize the Results ######Test_smc2##############################################################
 # You can plot the filtered estimate of the state and parametersx
 ############################################################################################################
 
+separation_point = simulated_data['time'].iloc[-1]-fday
 ###  Evolution of the model weights
 w_dthp = smc2_results['weight_dthp']  
-w_seir = smc2_results['weight_seir']
+w_sir = smc2_results['weight_sir']
 
 # Plotting
 fig, ax = plt.subplots(figsize=(17/3, 4))  
 ax.plot(w_dthp, label='Weight dthp', color='orange', linewidth=2, alpha=0.8)
-ax.plot(w_seir, label='Weight seir', color='dodgerblue', linewidth=2, alpha=0.8)
+ax.plot(w_sir, label='Weight sir', color='dodgerblue', linewidth=2, alpha=0.8)
 ax.grid(True, linestyle='--', alpha=0.7)  # Add grid with dashed lines
 ax.set_facecolor('whitesmoke')  # Add light background color to the plot area
 # Adding labels and title with improved fonts
 ax.set_xlabel('Time (days)', fontsize=18, fontweight='bold', labelpad=10)
 ax.set_ylabel('Model weights', fontsize=18, fontweight='bold', labelpad=10)
-ax.set_title('Scenario 3', fontsize=18, fontweight='bold', pad=15)
+ax.set_title('Scenario C', fontsize=18, fontweight='bold', pad=15)
 # Customize ticks
 ax.tick_params(axis='both', which='major', labelsize=16)
 ax.tick_params(axis='both', which='minor', labelsize=10)
@@ -224,21 +228,21 @@ plt.show()
 
 # Extract trajectories SIR model
 trajParticles_state_sir = smc2_results['traj_state_sir']
-matrix_dict_state = trace_smc(trajParticles_state)
+matrix_dict_state_sir = trace_smc(trajParticles_state_sir)
 trajParticles_theta_sir = smc2_results['traj_theta_sir']
 matrix_dict_theta_sir = trace_smc(trajParticles_theta_sir)
-g=np.median(matrix_dict_theta_sir['gamma'][:,-1]
+gamma=np.mean(matrix_dict_theta_sir['gamma'][:,-1])
 # Calculate the  reproduction number Rt and add it to the satate dict
-matrix_dict_state['Rt']=matrix_dict_state['B']/g
+matrix_dict_state_sir['Rt']=matrix_dict_state_sir['B']/gamma
 
 # Extract trajectories DTHP
 trajParticles_state_dthp = smc2_results['traj_state_dthp']
-matrix_dict_state = trace_smc(trajParticles_state)
+matrix_dict_state_dthp = trace_smc(trajParticles_state_dthp)
 trajParticles_theta_dthp = smc2_results['traj_theta_dthp']
 matrix_dict_theta_dthp = trace_smc(trajParticles_theta_dthp)
 
 # Compute trajectories model-averging based on the model weights
-matrix_dict_avg = compute_weighted_average(matrix_dict_dthp, matrix_dict_seir, w_dthp, w_seir)
+matrix_dict_state_avg = compute_model_average(matrix_dict_state_dthp, matrix_dict_state_sir, w_dthp, w_sir)
 
 
 # Plot incidence and time-varying reproduction number for the 3 models
@@ -248,11 +252,10 @@ titles = ['DTHP', 'SIR', 'MA']
 
 # Plot NI (top row)
 window = 1 ## increase if you want more smoothed plot
-separation_point = =len(simulated_data['time'])-fday
 for i, (method, matrix) in enumerate([
-    ('DTHP', matrix_dict_dthp['NI']),
-    ('SIR', matrix_dict_seir['NI']),
-    ('MA', matrix_dict_avg['NI'])
+    ('DTHP', matrix_dict_state_dthp['NI']),
+    ('SIR', matrix_dict_state_sir['NI']),
+    ('MA', matrix_dict_state_avg['NI'])
 ]):
     plot_smc(matrix, ax=axs[0, i], separation_point=separation_point,  window=window)
     # Add observed data for days 1 to `days`
@@ -283,9 +286,9 @@ for i, (method, matrix) in enumerate([
 
 # Plot Rt (bottom row)
 for i, (method, matrix) in enumerate([
-    ('DTHP', matrix_dict_dthp['Rt']),
-    ('SIR', matrix_dict_seir['Rt']),
-    ('MA', matrix_dict_avg['Rt'])
+    ('DTHP', matrix_dict_state_dthp['Rt']),
+    ('SIR', matrix_dict_state_sir['Rt']),
+    ('MA', matrix_dict_state_avg['Rt'])
 ]):
     # Plot SMC results
     plot_smc(matrix, ax=axs[1, i],separation_point=separation_point,  window=window)
@@ -307,7 +310,7 @@ for i, (method, matrix) in enumerate([
     axs[1, i].set_xlabel('Time (days)', fontsize=16, fontweight='bold')
     axs[1, i].set_ylim([0, 6])
     axs[1, i].tick_params(axis='both', which='major', labelsize=16)
-    axs[1, i].axvline(x=separation_point, color='black', linestyle='--', linewidth=2')
+    axs[1, i].axvline(x=separation_point, color='black', linestyle='--', linewidth=2)
 
 # Adjust layout to prevent overlapping and add horizontal spacing
 # Create legend elements
@@ -335,11 +338,10 @@ plt.subplots_adjust(wspace=0.05, hspace=0.1)
 
 
 
+
 #############################################################################
 # Plot for the parameter trajectories
 ##############################################################################
-
-# Example of labels 
 
 
 # Example label
@@ -347,11 +349,6 @@ L1 = [r'$\omega$', r'$\nu_{1}$', r'$\phi_1$']
 L2 = [r'$\gamma$', r'$\nu_{2}$', r'$\phi_2$']
 
 # Data sources
-trajParticles1 = smc2_results['traj_theta_dthp']
-trajParticles2 = smc2_results['traj_theta_seir']
-
-matrix_dict1 = merge_dataframes(trajParticles1)
-matrix_dict2 = merge_dataframes(trajParticles2)
 
 # Combined number of rows and columns for subplots
 nrows, ncols = 2, 3
@@ -361,7 +358,7 @@ fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(20, 8))
 axes = axes.flatten()
 
 # Iterate and plot for the first dataset
-for i, (state, matrix) in enumerate(matrix_dict1.items()):
+for i, (state, matrix) in enumerate(matrix_dict_theta_dthp.items()):
     ax = axes[i]  # Current axis
     plot_smc(matrix, ax=ax)   
     # Calculate 0.25 and 0.975 CIs
@@ -371,7 +368,7 @@ for i, (state, matrix) in enumerate(matrix_dict1.items()):
     ax.set_title(f'{L1[i]}= {median_estimate:.3f} (95%CrI: [{ci_025:.3f}, {ci_975:.3f}])', fontsize=18, fontweight='bold')
     ax.set_ylabel(L1[i], fontsize=25, fontweight='bold')
 # Iterate and plot for the second dataset
-for i, (state, matrix) in enumerate(matrix_dict2.items()):
+for i, (state, matrix) in enumerate(matrix_dict_theta_sir.items()):
     ax = axes[i + 3]  # Move to the second row
     plot_smc(matrix, ax=ax)
     # Calculate 0.25 and 0.975 CIs
@@ -382,7 +379,7 @@ for i, (state, matrix) in enumerate(matrix_dict2.items()):
     ax.set_title(f'{L2[i]}= {median_estimate:.3f} (95%CrI: [{ci_025:.3f}, {ci_975:.3f}])', fontsize=18, fontweight='bold')
     # Add a true value line for the first state in the second dataset if applicable
     if i == 0:
-        ax.axhline(true_theta[i + 1], color='orange', linestyle='--', linewidth=3, label='True Value')
+        ax.axhline(true_theta[i], color='orange', linestyle='--', linewidth=3, label='True Value')
         # ax.legend(fontsize=12)
     # Customize labels
     ax.set_xlabel('Time (days)', fontsize=16, fontweight='bold')
@@ -396,3 +393,4 @@ for ax in axes.flat:
 # Adjust layout and show the complete figure
 plt.tight_layout()
 plt.show()
+
